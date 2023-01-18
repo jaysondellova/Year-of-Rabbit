bce48e95469f319f97e9dd533531944d8fc623e5
Jayson Dellova <jayson.dellova2@gmail.com>
2023-01-18 21:51:35 +0800

    edit tokenomics

--- src/components/Tokenomics.js
+++ src/components/Tokenomics.js
@@ -11,9 +11,9 @@ const Tokenomics = () => {
           <h2>2% MARKETING</h2>
         </div>
         <div className="sell">
-          <h2>6% SELL</h2>
+          <h2>3% SELL</h2>
           <h2>1% LP</h2>
-          <h2>5% MARKETING</h2>
+          <h2>2% MARKETING</h2>
         </div>
       </div>
     </div>
